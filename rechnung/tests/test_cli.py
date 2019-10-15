59651de719a523a33370b2b6f1ffd2f34baea792
olf <olf@subsignal.org>
2019-10-15 21:48:19 +0200

    add print-stats test and fixture with test fixtures

--- rechnung/tests/test_cli.py
+++ rechnung/tests/test_cli.py
@@ -1,19 +1,17 @@
-import click
 import pytest
 import rechnung.cli as cli
 import rechnung.settings as settings
 
 from click.testing import CliRunner
+from shutil import copytree
 from pathlib import Path
-from mock import patch
-import os
 
 
 @pytest.fixture
 def cli_path(tmp_path):
     """
     Returnes the click.CommandGroup of cli.py holding all executeable commands,
-    but with the module withe variable "cwd" overwritten to a temporary path. This is 
+    but with the module withe variable "cwd" overwritten to a temporary path. This is
     required as the current working directory is determined by os.getcwd() on import
     olf rechnung.cli
     """
@@ -31,8 +29,21 @@ def initialized_path(tmp_path_factory):
     tmp_path.mkdir()
     cli.cwd = tmp_path
     runner = CliRunner()
-    result = runner.invoke(cli.cli1, ["init"])
+    runner.invoke(cli.cli1, ["init"])
     return tmp_path
+
+
+@pytest.fixture(scope="session")
+def cli_test_data_path(tmp_path_factory):
+    """
+    Returns a path where rechnung is initialized in order to verify correct creation
+    of directories and files
+    """
+    tmp_path = tmp_path_factory.getbasetemp()
+    cli_path = tmp_path / "rechnung_test_data"
+    copytree(Path("rechnung/tests/fixtures"), cli_path)
+    cli.cwd = cli_path
+    return cli.cli1, cli_path
 
 
 def test_init_exit_code(cli_path):
@@ -57,6 +68,15 @@ def test_init_files_directories(initialized_path):
         if setting_k.endswith("_dir"):
             expected_elements.append(setting_v)
     for element in initialized_path.iterdir():
-        print(f"x: {element.name}")
         expected_elements.remove(element.name)
     assert not len(expected_elements)
+
+
+def test_print_stats(cli_test_data_path):
+    cli1, path = cli_test_data_path
+    runner = CliRunner()
+    result = runner.invoke(cli1, ["print-stats"])
+    assert "108.66" in result.output
+
+
+#    assert False
