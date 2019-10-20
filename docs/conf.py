1e21a4fc69035feeba60fba7cdf3390d02514737
olf <olf@subsignal.org>
2019-10-20 17:11:16 +0200

    black docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'rechnung'
-copyright = '2019, Florian Rämisch, Paul Spooren'
-author = 'Florian Rämisch, Paul Spooren'
+project = "rechnung"
+copyright = "2019, Florian Rämisch, Paul Spooren"
+author = "Florian Rämisch, Paul Spooren"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,24 +27,24 @@ author = 'Florian Rämisch, Paul Spooren'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', ]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-master_doc = 'index'
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 html_theme_options = {
     "font_family": '"Avenir Next", Calibri, "PT Sans", sans-serif',
@@ -52,11 +52,11 @@ html_theme_options = {
     "font_size": "14px",
     "page_width": "980px",
     "sidebar_width": "225px",
-    "show_relbars": True
+    "show_relbars": True,
 }
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
